d83e9862420228872e6f4443374869fda54109d4
Zhang Kai Yu <yeannylam@gmail.com>
2015-09-12 20:59:58 +0800

    Initial Commit

---
+++ Kroman/Kroman.h
@@ -0,0 +1,19 @@
+//
+//  Kroman.h
+//  Kroman
+//
+//  Created by Kai Yu on 9/12/15.
+//  Copyright © 2015 Zhang Kai Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kroman.
+FOUNDATION_EXPORT double KromanVersionNumber;
+
+//! Project version string for Kroman.
+FOUNDATION_EXPORT const unsigned char KromanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kroman/PublicHeader.h>
+
+
